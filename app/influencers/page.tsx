800ebeee1fbdf33e04084c35da542fdeb28f2336
Muhammad Alam <malam53345@gmail.com>
2024-01-04 16:18:19 +0500

    updated

--- app/influencers/page.tsx
+++ app/influencers/page.tsx
@@ -3,10 +3,10 @@ export default function Influencers() {
     return (
         <div className='py-[3rem] md:py-[5rem]'>
             <div className="container flex-col md:flex-row gap-[2rem] items-center">
-                <div className="flex flex-col gap-[1rem]">
-                    <h1 className="text-[40px] font-[600] leading-[50px] text-[--blackColor]">Create your Freedom.</h1>
-                    <p className="text-[16px] font-[500] leading-[24px] text-[#2D3648] w-full md:w-[60%]">La primera aplicación con la cual podrás colaborar con marcas, agencias y otros creadores.</p>
-                    <div className="flex flex-wrap items-center gap-[1rem]">
+                <div className="flex flex-col gap-[1rem] items-center md:items-start">
+                    <h1 className="text-[40px] font-[600] leading-[50px] text-[--blackColor] text-center md:text-left">Create your Freedom.</h1>
+                    <p className="text-[16px] font-[500] leading-[24px] text-[#2D3648] w-full md:w-[60%] text-center md:text-left">La primera aplicación con la cual podrás colaborar con marcas, agencias y otros creadores.</p>
+                    <div className="flex flex-wrap items-center gap-[1rem] justify-center">
                         <Image src="/images/apple.svg" width={150} height={52} alt="apple"></Image>
                         <Image src="/images/playstore.svg" width={150} height={52} alt="playstore"></Image>
                     </div>
